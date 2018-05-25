c5ed3eb2ec9c6d7406f2a1fc3f09f53121ae135e
MartinSalazarC <39386411+MartinSalazarC@users.noreply.github.com>
2018-05-24 23:14:29 -0500

    Add files via upload

--- consultareclamo.php
+++ consultareclamo.php
@@ -93,6 +93,7 @@ function f_conreclamo($codigo)
 	$c_detRec = "call sp_con_reclamo_x_cod(".$codigo.")";
 	$r_detRec = mysqli_query($link, $c_detRec);
 	$d_detRec = mysqli_fetch_array($r_detRec);
+	desconectar_db();
 	
 	$divCpo = "
 		<div class='card border-warning'>
@@ -100,206 +101,140 @@ function f_conreclamo($codigo)
 				<b>Registro presentado</b>
 			</div>
 			<div class='card-body'>
-				<h5>
+				<h6>
 					<b>Identificaci&oacute;n del consumidor que presenta el reclamo</b>
-				</h5>
+				</h6>
 				<form name = 'frmDatConsumidor' action='' method='post' class='justify-content-center'>
 					<div class='form-group row'>
 						<div class='col-12 col-sm-5 col-lg-4 mb-1'>
-							<label for='txtTipDoc' class='textDescripcion01'>Tipo de Documento *</label>
-							<input type='text' name='txtTipDoc' id='txtTipDoc' class='form-control form-control-sm textContenido01' value='".$d_detRec[1]."' >
+							<label for='txtTipDoc' class='textDescripcion01'>Tipo de Documento </label>
+							<input type='text' name='txtTipDoc' id='txtTipDoc' class='form-control form-control-sm textContenido01' value='".$d_detRec[1]."' disabled>
 						</div>
 						<div class='col-12 col-sm-7 col-lg-4 mb-1'>
-							<label for='txtNroDoc' class='textDescripcion01'>N&uacute;mero *</label>
-							<input type='number' name='txtNroDoc' id='txtNroDoc' class='form-control form-control-sm textContenido01' value='".$d_detRec[2]."' >
+							<label for='txtNroDoc' class='textDescripcion01'>N&uacute;mero </label>
+							<input type='number' name='txtNroDoc' id='txtNroDoc' class='form-control form-control-sm textContenido01' value='".$d_detRec[2]."' disabled>
 						</div>
 					</div>
 					<div class='form-group row'>
 						<div class='col-12 col-md-6 mb-1'>
-							<label for='txtApellidos' class='textDescripcion01'>Apellidos *</label>
-							<input type='text' name='txtApellidos' id='txtApellidos' class='form-control form-control-sm textContenido01' value='".$d_detRec[3]."' >
+							<label for='txtApellidos' class='textDescripcion01'>Apellidos </label>
+							<input type='text' name='txtApellidos' id='txtApellidos' class='form-control form-control-sm textContenido01' value='".$d_detRec[3]."' disabled>
 						</div>
 						<div class='col-12 col-md-6 mb-1'>
-							<label for='txtNombres' class='textDescripcion01'>Nombres * </label>
-							<input type='text' name='txtNombres' id='txtNombres' class='form-control form-control-sm textContenido01' value='".$d_detRec[4]."' >
+							<label for='txtNombres' class='textDescripcion01'>Nombres  </label>
+							<input type='text' name='txtNombres' id='txtNombres' class='form-control form-control-sm textContenido01' value='".$d_detRec[4]."' disabled>
 						</div>
 					</div>
 					<div class='form-group row'>
 						<div class='col-12 col-md-12 mb-1'>
-							<label for='txtDireccion' class='textDescripcion01'>Direcci&oacute;n * </label>
-							<input type='text' name='txtDireccion' id='txtDireccion' class='form-control form-control-sm textContenido01' value='".$d_detRec[5]."' >
+							<label for='txtDireccion' class='textDescripcion01'>Direcci&oacute;n  </label>
+							<input type='text' name='txtDireccion' id='txtDireccion' class='form-control form-control-sm textContenido01' value='".$d_detRec[5]."' disabled>
 						</div>
 					</div>
 					<div class='form-group row'>
 						<div class='col-12 col-md-3 mb-1'>
 							<label for='txtTelefono' class='textDescripcion01'>Tel&eacute;fono</label>
-							<input type='number' name='txtTelefono' id='txtTelefono' class='form-control form-control-sm textContenido01' value='".$d_detRec[6]."' >
+							<input type='number' name='txtTelefono' id='txtTelefono' class='form-control form-control-sm textContenido01' value='".$d_detRec[6]."' disabled>
 						</div>
 						<div class='col-12 col-md-9 mb-1'>
 							<label for='txtCorreo' class='textDescripcion01'>Correo electr&oacute;nico</label>
-							<input type='email' name='txtCorreo' id='txtCorreo' class='form-control form-control-sm textContenido01min' value='".$d_detRec[7]."' >
+							<input type='email' name='txtCorreo' id='txtCorreo' class='form-control form-control-sm textContenido01min' value='".$d_detRec[7]."' disabled>
 						</div>
-					</div>
-					<div class='form-group row'>
+					</div>";
+	
+	$menor = "SI";
+	if ($d_detRec[8] == "N")
+		$menor = "NO";
+	
+	$divCpo.= "		<div class='form-group row'>
 						<div class='col-12 col-md-2 mb-1'>
 							<label for='txtIndMenor' class='textDescripcion01'>Menor de edad?</label>
-							<input type='text' name='txtIndMenor' id='txtIndMenor' class='form-control form-control-sm textContenido01' value='' >
+							<input type='text' name='txtIndMenor' id='txtIndMenor' class='form-control form-control-sm textContenido01' value='".$menor."' disabled>
 						</div>
 						<div class='col-12 col-md-10 mb-1'>
 							<label for='txtNomPadres' class='textDescripcion01'>Nombre del Padre o Apoderado</label>
-							<input type='text' name='txtNomPadres' id='txtNomPadres' class='form-control form-control-sm textContenido01' value='' >
+							<input type='text' name='txtNomPadres' id='txtNomPadres' class='form-control form-control-sm textContenido01' value='".$d_detRec[9]."' disabled>
 						</div>
 					</div>
 				</form>
-				<h5>
+				<h6>
 					<b>Datos del producto o servicio contratado</b>
-				</h5>
+				</h6>
 				<form name = 'frmDatProducto' action='' method='post' class='justify-content-center'>
 					<div class='form-group row'>
 						<div class='col-12 col-sm-10 mb-1'>
-							<label for='txtLocal' class='textDescripcion01'>Local *</label>
-							<input type='text' name='txtLocal' id='txtLocal' class='form-control form-control-sm textContenido01' value='' >
+							<label for='txtLocal' class='textDescripcion01'>Local </label>
+							<input type='text' name='txtLocal' id='txtLocal' class='form-control form-control-sm textContenido01' value='".$d_detRec[10]."' disabled>
 						</div>
 					</div>
 					<div class='form-group row'>
 						<div class='col-12 col-md-4'>
-							<label for='txtTipoBien' class='textDescripcion01'>Tipo de bien contratado *</label>
-							<input type='text' name='txtTipoBien' id='txtTipoBien' class='form-control form-control-sm textContenido01' value='' >
+							<label for='txtTipoBien' class='textDescripcion01'>Tipo de bien contratado </label>
+							<input type='text' name='txtTipoBien' id='txtTipoBien' class='form-control form-control-sm textContenido01' value='".$d_detRec[11]."' disabled>
 							<label for='txtMontoReclamado' class='textDescripcion01 mt-4 mb-3'>Monto Reclamado</label>
-							<input type='number' name='txtMontoReclamado' id='txtMontoReclamado' class='form-control form-control-sm textContenido01' value=''  >
+							<input type='number' name='txtMontoReclamado' id='txtMontoReclamado' class='form-control form-control-sm textContenido01' value='".$d_detRec[12]."' disabled>
 						</div>
 						<div class='col-12 col-md-8'>
 							<label for='txtDescripcion' class='textDescripcion01'>Descripci&Oacute;n:</label>
-							<textarea name='txtDescripcion' id='txtDescripcion' rows='6' class='form-control form-control-sm textContenido01' ></textarea>
+							<textarea name='txtDescripcion' id='txtDescripcion' rows='6' class='form-control form-control-sm textContenido01' readonly>".$d_detRec[13]."</textarea>
 						</div>
 					</div>
 				</form>
-				<h5>
+				<h6>
 					<b>Detalle de la reclamaci&oacute;n y pedido del consumidor</b>
-				</h5>
+				</h6>
 				<form name = 'frmDatReclamo' action='' method='post' class='justify-content-center'>
 					<div class='form-group row'>
 						<div class='col-12 col-sm-3 mb-1'>
-							<label for='txtTipo' class='textDescripcion01'>Tipo Pedido *</label>
-							<input type='text' name='txtTipo' id='txtTipo' class='form-control form-control-sm textContenido01' value='' >
+							<label for='txtTipo' class='textDescripcion01'>Tipo Pedido </label>
+							<input type='text' name='txtTipo' id='txtTipo' class='form-control form-control-sm textContenido01' value='".$d_detRec[14]."' disabled>
 						</div>
 					</div>
 					<div class='form-group row'>
 						<div class='col-12 col-md-6'>
-							<label for='txtDetalle' class='textDescripcion01'>Detalle del Reclamo *</label>
-							<textarea name='txtDetalle' id='txtDetalle' rows='6' class='form-control form-control-sm textContenido01' ></textarea>
+							<label for='txtDetalle' class='textDescripcion01'>Detalle del Reclamo </label>
+							<textarea name='txtDetalle' id='txtDetalle' rows='6' class='form-control form-control-sm textContenido01' readonly>".$d_detRec[15]."</textarea>
 						</div>
 						<div class='col-12 col-md-6'>
 							<label for='txtPedido' class='textDescripcion01'>Pedido </label>
-							<textarea name='txtPedido' id='txtPedido' rows='6' class='form-control form-control-sm textContenido01' ></textarea>
+							<textarea name='txtPedido' id='txtPedido' rows='6' class='form-control form-control-sm textContenido01' readonly>".$d_detRec[16]."</textarea>
 						</div>
 					</div>
 				</form>
 			</div>
 		</div>
-		<br><br>";
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	$divCpo.= "
-			<table  width='800' border='0' align='center'>
-				<tr>
-					<td width='40%'></td>
-					<td width='60%'></td>
-				</tr>
-				<tr>
-					<td class='textCabTabla' colspan='2'>Datos de la persona que realiza el reclamo</td>
-				</tr>
-				<tr>
-					<td align='right' class='textDescripcion'>Fecha de registro del reclamo:</td>
-					<td class='textDetalle'>".$d_detRec[17]."</td>
-				</tr>
-				<tr>
-				<td align='right' class='textDescripcion'>Menor de edad?</td>
-				<td class='textDetalle'>";
-				
-			if ($d_detRec[8] == "N")
-				$divCpo.= "NO";
-			else
-				$divCpo.= "SI";
-				
-				$divCpo.= "</td>
-				</tr>
-				<tr>
-				<td align='right' class='textDescripcion'>Nombre del padre o la madre en caso de menor de edad</td>
-				<td class='textDetalle'>".$d_detRec[9]."</td>
-				</tr>
-				<tr>
-					<td colspan='2'>&nbsp;</td>
-				</tr>
-				<tr>
-					<td class='textCabTabla' colspan='2'>Datos del producto o servicio contratado</td>
-				</tr>
-				<tr>
-					<td align='right' class='textDescripcion'>Local:</td>
-					<td class='textDetalle'>".$d_detRec[10]."</td>
-				</tr>
-				<tr>
-					<td align='right' class='textDescripcion'>Tipo de bien contratado:</td>
-					<td class='textDetalle'>".$d_detRec[11]."</td>
-				</tr>
-				<tr>
-					<td align='right' class='textDescripcion'>Monto Reclamado:</td>
-					<td class='textDetalle'>".$d_detRec[12]."</td>
-				</tr>
-				<tr>
-					<td align='right' class='textDescripcion'>Descripci&oacute;n:</td>
-					<td class='textDetalle'>".$d_detRec[13]."</td>
-				</tr>
-				<tr>
-					<td colspan='2'>&nbsp;</td>
-				</tr>
-				<tr>
-					<td class='textCabTabla' colspan='2'>Detalle de la reclamaci&oacute;n y pedido del consumidor</td>                        
-				</tr>
-				<tr>
-					<td align='right' class='textDescripcion'>Tipo:</td>
-					<td class='textDetalle'>".$d_detRec[14]."</td>
-				</tr>
-				<tr>
-					<td align='right' class='textDescripcion'>Detalle del reclamo:</td>
-					<td class='textDetalle'>".$d_detRec[15]."</td>
-				</tr>
-				<tr>
-					<td align='right' class='textDescripcion'>Pedido:</td>
-					<td class='textDetalle'>".$d_detRec[16]."</td>
-				</tr>
-				<tr>
-					<td colspan='2'>&nbsp;</td>
-				</tr>
-				<tr>
-					<td class='textCabTabla' colspan='2'>Respuesta</td>
-				</tr> 
-				<tr>
-					<td align='right' class='textDescripcion'>Fecha de la respuesta:</td>
-					<td class='textDetalle'>".$d_detRec[17]."</td>
-				</tr>
-				<tr>
-					<td align='right' class='textDescripcion'>Respuesta:</td>
-					<td class='textDetalle'>".$d_detRec[18]."</td>
-				</tr>
-				<tr>
-					<td colspan='2'><hr></td>
-				</tr>
-			</table>";
+		<br>
+		<div class='card border-warning'>
+			<div class='card-body'>
+				<h6>
+					<b>Respuesta del Reclamo</b>
+				</h6>
+				<form name = 'frmRptaRec' action='' method='post' class='justify-content-center'>
+					<div class='form-group row justify-content-around'>
+						<div class='col-12 col-sm-5 mb-1'>
+							<label for='txtTipDoc' class='textDescripcion01'>Fecha de Registro</label>
+							<input type='text' name='txtTipDoc' id='txtTipDoc' class='form-control form-control-sm textContenido01' value='".$d_detRec[17]."' disabled>
+						</div>
+						<div class='col-12 col-sm-5 mb-1'>
+							<label for='txtNroDoc' class='textDescripcion01'>Fecha de Respuesta</label>
+							<input type='number' name='txtNroDoc' id='txtNroDoc' class='form-control form-control-sm textContenido01' value='".$d_detRec[18]."' disabled>
+						</div>
+					</div>
+					<div class='form-group row justify-content-center'>
+						<div class='col-12 col-md-11'>
+							<label for='txtDescripcion' class='textDescripcion01'>Descripci&Oacute;n:</label>
+							<textarea name='txtDescripcion' id='txtDescripcion' rows='6' class='form-control form-control-sm textContenido01' readonly>".$d_detRec[19]."</textarea>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+		<br>";
 		
-		desconectar_db();
+		$divFrm = "";
 		
 		$respuesta->Assign("divCpo", "innerHTML", $divCpo);
+		$respuesta->Assign("divFrm", "innerHTML", $divFrm);
 		return $respuesta;
 }
 
@@ -351,35 +286,37 @@ $xajax->configure('javascript URI','xajax/');
 							</div>
 						</div>
 						<br>
-						<div class='row justify-content-center mt-2'>
-							<div class='col-10 col-md-8 text-center'>
-								<form action="" method="post" name="frmConReclamo" class="justify-content-center">	
-									<div class='form-group row'>
-										<div class='col-12 col-md-5 col-lg-5 text-left'>
-											<label for='slcTipDoc' class='textDescripcion01'>Tipo de Documento</label>
-											<select name='slcTipDoc' id='slcTipDoc' class='form-control textContenido01 form-control-sm'>
-												<option value=''>[Documento]</option>
-									<?php
-									$link = conectar_db();
-									$c_tipdoc = "call sp_con_mae_tabla_det_x_cod_tabla('P001')";
-									$r_tipdoc = mysqli_query($link, $c_tipdoc);
-									while ($d_tipdoc = mysqli_fetch_array($r_tipdoc))
-									{
-										echo "	<option value='".$d_tipdoc[0]."'>".$d_tipdoc[4]."</option>";
-									}
-									desconectar_db();
-									?>
-											</select>
+						<div id="divFrm">
+							<div class='row justify-content-center mt-2'>
+								<div class='col-10 col-md-8 text-center'>
+									<form action="" method="post" name="frmConReclamo" class="justify-content-center">	
+										<div class='form-group row'>
+											<div class='col-12 col-md-5 col-lg-5 text-left'>
+												<label for='slcTipDoc' class='textDescripcion01'>Tipo de Documento</label>
+												<select name='slcTipDoc' id='slcTipDoc' class='form-control textContenido01 form-control-sm'>
+													<option value=''>[Documento]</option>
+										<?php
+										$link = conectar_db();
+										$c_tipdoc = "call sp_con_mae_tabla_det_x_cod_tabla('P001')";
+										$r_tipdoc = mysqli_query($link, $c_tipdoc);
+										while ($d_tipdoc = mysqli_fetch_array($r_tipdoc))
+										{
+											echo "	<option value='".$d_tipdoc[0]."'>".$d_tipdoc[4]."</option>";
+										}
+										desconectar_db();
+										?>
+												</select>
+											</div>
+											<div class='col-12 col-md-5 col-lg-5 text-left'>
+												<label for='txtNroDoc' class='textDescripcion01'>N&uacute;mero de Documento</label>
+												<input type='number' name='txtNroDoc' id='txtNroDoc' class='form-control form-control-sm textContenido01' maxlength='20' onKeyPress='return onlyNumeric(event);' placeholder='N&uacute;mero' >
+											</div>
+											<div class='col-12 col-md-2 col-lg-2 mt-4 text-center'>
+												<button name='cmdBuscar' type='button' class='btn btn-success textBotonMin mr-3' title='Buscar' onClick="xajax_f_listareclamo(xajax.getFormValues(frmConReclamo));" >Consultar</button>
+											</div>
 										</div>
-										<div class='col-12 col-md-5 col-lg-5 text-left'>
-											<label for='txtNroDoc' class='textDescripcion01'>N&uacute;mero de Documento</label>
-											<input type='number' name='txtNroDoc' id='txtNroDoc' class='form-control form-control-sm textContenido01' maxlength='20' onKeyPress='return onlyNumeric(event);' placeholder='N&uacute;mero' >
-										</div>
-										<div class='col-12 col-md-2 col-lg-2 mt-4 text-center'>
-											<button name='cmdBuscar' type='button' class='btn btn-success textBotonMin mr-3' title='Buscar' onClick="xajax_f_listareclamo(xajax.getFormValues(frmConReclamo));" >Consultar</button>
-										</div>
-									</div>
-								</form>
+									</form>
+								</div>
 							</div>
 						</div>
 						<div id="divCpo" class="justify-content-center"></div>
